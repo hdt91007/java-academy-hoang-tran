1a52df596b5c8a2c6555aed08bddefa929c4a3ca
Hdt91007 <hdt91007@gmail.com>
2025-09-24 14:18:23 -0700

    variableapp excerxxies

--- RemseyDemo/untitled/src/main/java/com/pluralsite/Main.java
+++ RemseyDemo/untitled/src/main/java/com/pluralsite/Main.java
@@ -5,5 +5,38 @@ package com.pluralsite;
 public class Main {
     public static void main(String[] args) {
         System.out.println(" the richee speed run goes crazy ");
+        //Primitive variables
+        int age = 24;
+        String favartist = "Shishishishi";
+        String colour = "Blue";
+        String firstname = "Hoang";
+        String lastname = "Tran";
+        System.out.println("My name is " + firstname + " " + lastname);
+        System.out.println("My favorite artist is " + favartist);
+        System.out.println("My favorite color is " + colour);
+        System.out.println("I am " + age + "years old");
+
+        boolean Eatyet = false;
+        boolean ishappy = true;
+        if (Eatyet == true) {
+            System.out.println("I already ate");
+        }
+        else System.out.println("Not yet ");
+
+        if (Eatyet && ishappy) {
+            System.out.println(" thats good");
+        }
+        else if (!Eatyet && !ishappy) {
+            System.out.println(" you could be unhappy cause hungry");
+        }
+        else if (Eatyet && !ishappy) {
+            System.out.println(" atleast your not hungry");
+            }
+        else {
+            System.out.println(" your happy but you should eat");
+
+        }
+
+        }
+
     }
-}
---
+++ Workbook1/StringApplication/src/main/java/com/pluralsite/VariableApp.java
@@ -0,0 +1,32 @@
+package com.pluralsite;
+
+
+import java.time.DayOfWeek;
+
+public class VariableApp {
+    public static void main(String[] args) {
+        String Favoritecolor = "blue";
+        int YUUYear = 2025;
+        char MiddleInital = 'D';
+        boolean Havepets = false;
+        String NiceMessage = "I hope your having a good day";
+        System.out.println(Favoritecolor + " is my favorite color");
+        System.out.println(Favoritecolor + " I started year up united in " + YUUYear);
+        System.out.println( "Those with the Middle name " + MiddleInital + " will change the world");
+        if(Havepets == false) System.out.println("I dont have a pet");
+        else  System.out.println("I have a dog");
+
+        System.out.println(String)NiceMessage);
+        //Declare AND initialize variables for:
+        //2. The number of days in a week
+        //3. The price of coffee ($4.99)
+        //4. Your favorite letter
+        //5. Whether it's raining (true/false)
+        //6. Print all values in complete sentences
+
+        // declare variables here
+        // then code solution
+        // then use System.out.println() to display results
+        // ex: System.out.println("The answer is " + answer);d
+        int Daysinaweek = 7
+}
