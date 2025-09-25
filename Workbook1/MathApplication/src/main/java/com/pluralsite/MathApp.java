defaf3f08833328ab2d26d57b216863cb88d716c
Hdt91007 <hdt91007@gmail.com>
2025-09-24 18:56:16 -0700

    work on ex3

---
+++ Workbook1/MathApplication/src/main/java/com/pluralsite/MathApp.java
@@ -0,0 +1,30 @@
+package com.pluralsite;
+
+public class MathApp {
+    public static void main(String[] args) {
+        // Question 1:
+        // declare variables here
+        // then code solution
+        // then use System.out.println() to display results
+        // ex: System.out.println("The answer is " + answer);
+        double Bobsalary = 2319313;
+        double Garysalary = 1232144;
+
+        double highestsalary = Math.max(Bobsalary, Garysalary);
+        System.out.println("The highest salary between Gary and Bob is $" + highestsalary);
+
+        double Truckprice = 54769.12;
+        double carprice = 65492.86;
+        double lowestprice = Math.min(Truckprice,carprice);
+        System.out.println("The cheaper vehicle cost is $" + lowestprice);
+        double Radius = 23.3;
+        double AreaofCircle = Math.PI * (Radius * Radius);
+        System.out.println("The area of a circle with a radius of " + Radius + "inches is "+ AreaofCircle);
+        double variable1 =
+
+
+
+
+
+    }
+}
---
+++ Workbook1/StringApplication/src/main/java/com/pluralsite/Reciept.java
@@ -0,0 +1,15 @@
+package com.pluralsite;
+
+public class Reciept {
+    public static void main(String[] args) {
+//1.Create variables for an item name (String) and item price (double)
+//2. Create a quantity variable (int)
+//3. Build and print: EX. "You bought 3 apples for $7.10"
+        String Pproduct =" Pokemon Blisters";
+        Double PokemonBlister = 70.24;
+                int BlisterStock = 13;
+
+        System.out.println("You bought " + BlisterStock + Pproduct + " for $" + BlisterStock );
+
+    }
+}
