d5c6ba623cc46f9061b794c185a5d5627056cd24
Hdt91007 <hdt91007@gmail.com>
2025-09-26 11:20:19 -0700

    Sandwich Shop ex p1

---
+++ Workbook1/SandwichShop/src/main/java/com/pluralsite/SandwichShop.java
@@ -0,0 +1,44 @@
+package com.pluralsite;
+
+import java.util.Scanner;
+
+public class SandwichShop {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+    double discount = 0;
+    double price = 0;
+
+        System.out.println("Type if you would want a Small sandwich or Large sandwich");
+    String SandwichSize = scanner.nextLine();
+
+        System.out.println("Type out your to see if you qualify for a discount");
+    int Age = scanner.nextInt();
+        if (Age <= 17) {
+        discount = .10;
+        System.out.println("your discount is 10% ");
+    }
+
+        else if (Age >= 65) {  discount = .20;
+        System.out.println("your discount is 20%");
+    }
+
+
+    //Why do you cant you create a varaible inside an if
+        if (SandwichSize.equals("Small"))
+    {
+        price = 5.45;
+    }
+    //Why doesnt this work *else if (SandwichSize == "Large")*
+        else if (SandwichSize.equals("Large"))
+    {
+        price = 8.95;
+    }
+    double total = price*(1-discount);
+
+            System.out.println("Your total for a " + SandwichSize + " Sandiwich is $" + String.format("%.2f",(total)));
+
+
+
+}
+}
