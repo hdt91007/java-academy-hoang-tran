687fd5a3a6261bd4888831eb68a7699bb122da78
Hoang Tran <narfmcfarf@gmail.com>
2025-10-06 07:30:52 -0700

    tdit

---
+++ Workbook2/RollTheDice/src/com/pluralsight/Dice.java
@@ -0,0 +1,13 @@
+package com.pluralsight;
+
+import java.security.PublicKey;
+
+public class Dice {
+
+
+public static Dice deno() {
+
+   t int randomNumber = (int) (Math.random() * maxValue) + minValue;
+}
+
+}
