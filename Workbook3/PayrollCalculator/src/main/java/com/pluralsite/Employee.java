43d84d7013ceee7d6365994410f32299f64e7332
Hdt91007 <hdt91007@gmail.com>
2025-10-06 14:55:37 -0700

    payroll wip

---
+++ Workbook3/PayrollCalculator/src/main/java/com/pluralsite/Employee.java
@@ -0,0 +1,57 @@
+package com.pluralsite;
+
+public class Employee {
+
+        private int employeeid ;
+        private String name ;
+        private int hoursWorked ;
+        private double payRate;
+        private double grosspay;
+
+    public int getEmployeeid() {
+        return employeeid;
+    }
+
+    public void setEmployeeid(int employeeid) {
+        this.employeeid = employeeid;
+    }
+
+    public int getHoursWorked() {
+        return hoursWorked;
+    }
+
+    public void setHoursWorked(int hoursWorked) {
+        this.hoursWorked = hoursWorked;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getPayRate() {
+        return payRate;
+    }
+
+    public void setPayRate(double payRate) {
+        this.payRate = payRate;
+    }
+public void getgrosspay(){
+        this.grosspay = payRate*hoursWorked;
+    }
+
+    public Employee(int employeeid, String name, int hoursWorked, double payRate) {
+        this.employeeid = employeeid;
+        this.name = name;
+        this.hoursWorked = hoursWorked;
+        this.payRate = payRate;
+
+
+    }
+
+}
+
+
--- Workbook3/PayrollCalculator/src/main/java/com/pluralsite/Main.java
+++ Workbook3/PayrollCalculator/src/main/java/com/pluralsite/Main.java
@@ -1,17 +1,18 @@
 package com.pluralsite;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+    public static void main(String[] args) throws FileNotFoundException {
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        FileReader fileReader = new FileReader("Payroll.cvs");
+        BufferedReader bufReader = new BufferedReader(fileReader);
+
+
+        //Employee;
         }
     }
-}
