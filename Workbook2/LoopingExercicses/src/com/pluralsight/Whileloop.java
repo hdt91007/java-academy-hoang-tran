7012e532950e111132340fc292bd56da8cba48cb
Hoang Tran <narfmcfarf@gmail.com>
2025-10-05 17:16:50 -0700

    Looping Excercise

---
+++ Workbook2/LoopingExercicses/src/com/pluralsight/Whileloop.java
@@ -0,0 +1,16 @@
+package com.pluralsight;
+
+public class Whileloop {
+
+
+    public static void main(String[] args) {
+        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
+        // to see how IntelliJ IDEA suggests fixing it.
+
+        for (int i = 1; i <= 5; i++) {
+
+            System.out.println(i + ". I love java");
+        }
+
+    }
+}
