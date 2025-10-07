c82bf2c4d045f1bf58574a8b6f427cfd1f7ccf5f
Hoang Tran <narfmcfarf@gmail.com>
2025-10-07 00:46:57 -0700

    Work on BufferRead ???

--- Workbook3/PayrollCalculator/src/main/java/com/pluralsite/Main.java
+++ Workbook3/PayrollCalculator/src/main/java/com/pluralsite/Main.java
@@ -4,6 +4,7 @@ import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.Arrays;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
@@ -13,16 +14,18 @@ public class Main {
         try {
                 BufferedReader bufReader = new BufferedReader(new FileReader("src/CSVfile/Payroll.csv"));
             String EmployString;
-
+            String[] CsvData //= new String[0]
+             ;
             while (( EmployString = bufReader.readLine()) != null ){
-                System.out.println(EmployString);
+                //CsvData = EmployString.trim().split("|");
+                System.out.println(Arrays.toString(EmployString.trim().split("|")));
+                String EmployeeInfo = EmployString;
+                System.out.println(EmployeeInfo);
             }
-
 
         } catch (IOException e) {
             System.out.println("womp womp");
             throw new RuntimeException(e);
-
         }
 
     }
