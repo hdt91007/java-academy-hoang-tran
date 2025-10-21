6a16a27d804fbd20ee8f4a2ce0db757690f6f7ca
Hdt91007 <hdt91007@gmail.com>
2025-10-21 10:36:25 -0700

    hotel operations wb

---
+++ Workbook4/HotelOperations/HotelOperations/src/main/java/com/pluralsight/Main.java
@@ -0,0 +1,16 @@
+package com.pluralsight;
+
+import java.util.Random;
+import java.util.random.RandomGenerator;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(Math.random()/3*10);
+
+        for (int i = 0; i < 100; i++) {
+
+        }
+    }
+}
