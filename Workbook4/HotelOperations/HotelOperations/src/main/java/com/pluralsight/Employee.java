b65e64009709612d4249896c673e76ecad162b9d
Hdt91007 <hdt91007@gmail.com>
2025-10-21 13:36:22 -0700

    Coded Gettotalpay, Overtime hours , Regular hours

--- Workbook4/HotelOperations/HotelOperations/src/main/java/com/pluralsight/Employee.java
+++ Workbook4/HotelOperations/HotelOperations/src/main/java/com/pluralsight/Employee.java
@@ -6,4 +6,86 @@ public class Employee {
     private String department;
     private Double payRate;
     private Double hoursWorked;
-}
+    private Double OvertimeWorked;
+    private Double regularHoursWorked;
+    private Double Otpay;
+    private double normalpay;
+
+    public Employee(Double employeeId, String name, String department, Double payRate) {
+        this.employeeId = employeeId;
+        this.name = name;
+        this.department = department;
+        this.payRate = payRate;
+
+    }
+
+
+    public Double getEmployeeId() {
+        return employeeId;
+    }
+
+    public void setEmployeeId(Double employeeId) {
+        this.employeeId = employeeId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDepartment() {
+        return department;
+    }
+
+    public void setDepartment(String department) {
+        this.department = department;
+    }
+
+    public Double getPayRate() {
+        return payRate;
+    }
+
+    public void setPayRate(Double payRate) {
+        this.payRate = payRate;
+    }
+
+    public Double getHoursWorked() {
+        return hoursWorked;
+    }
+
+    public void setHoursWorked(Double hoursWorked) {
+        this.hoursWorked = hoursWorked;
+    }
+
+    public double getRegularhours() {
+
+        if (this.hoursWorked <= 40) {
+            return this.hoursWorked;
+        } else {
+            return 40;
+        }
+    }
+
+    public double getOvertimeWorked() {
+        if (this.hoursWorked > 40) {
+            this.OvertimeWorked = this.hoursWorked - 40;
+            return OvertimeWorked;
+        } else {
+            return 0;
+        }
+    }
+
+    public double getTotalpay() {
+        if (this.hoursWorked > 40) {
+            this.Otpay = getOvertimeWorked() * this.payRate * 1.5;
+            this.normalpay = this.payRate * 40;}
+        else { this.normalpay = payRate * getRegularhours(); }
+
+            return Otpay + normalpay;
+        }
+
+    }
+
--- Workbook4/HotelOperations/HotelOperations/src/main/java/com/pluralsight/Reservation.java
+++ Workbook4/HotelOperations/HotelOperations/src/main/java/com/pluralsight/Reservation.java
@@ -9,10 +9,49 @@ public class Reservation {
     private boolean isWeekend;
     private double Reservationtotal;
 
+    public String getRoomType() {
+        return RoomType;
+    }
+
+    public void setRoomType(String roomType) {
+        RoomType = roomType;
+    }
+
+    public Double getPrice() {
+        if (this.RoomType.equalsIgnoreCase("King")) {
+            this.Price = 139.00;
+        } else {
+            this.setPrice(124.00);
+        }
+
+        return Price;
+    }
 
 
+    public void setPrice(Double price) {
+        Price = price;
+    }
 
+    public int getNumberOfNights() {
+        return NumberOfNights;
+    }
 
+    public void setNumberOfNights(int numberOfNights) {
+        NumberOfNights = numberOfNights;
+    }
 
+    public boolean isWeekend() {
+        return isWeekend;
+    }
 
+    public void setWeekend(boolean weekend) {
+        isWeekend = weekend;
+    }
+
+    public double getReservationtotal() {
+        if (isWeekend() == true) {
+            this.Price = this.Price * 1.10;
+        }
+        return Price;
+    }
 }
